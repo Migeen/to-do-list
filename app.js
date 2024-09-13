f6efd5faed5f1f61d9facb219e7b8a901577ed92
Migeen Lamichhane Magar <migeen999@gmail.com>
2024-09-13 22:46:30 +0545

    code refractored

---
+++ app.js
@@ -0,0 +1,45 @@
+const addBtn = document.querySelector('#add');
+const delSvg = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#424242"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>'
+const checkSvg = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#424242"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>'
+// Implementing an Event Handler function for invoking a button click
+
+addBtn.addEventListener('click', () => {
+
+    const parent = document.querySelector('.List');
+    let task = document.querySelector('input').value;
+
+    //creating elements
+    let div = document.createElement('div');
+    div.classList.add('task');
+
+    let content = document.createElement('p');
+    content.classList.add('p');
+    content.innerHTML = task;
+
+    let btnDiv = document.createElement('Div');
+    btnDiv.classList.add('btns');
+
+    let delBtn = document.createElement('Button');
+    delBtn.classList.add('del');
+    delBtn.innerHTML = delSvg;
+
+    let checkBtn = document.createElement('Button');
+    checkBtn.classList.add('check');
+    checkBtn.innerHTML = checkSvg
+
+    let message = document.querySelector('.message');
+    
+    if(message.classList.contains('message1')){
+
+        message.classList.remove('.message');
+    }else{
+        message.classList.add('message1')
+    }
+
+    div.appendChild(content);
+    div.appendChild(btnDiv);
+    btnDiv.appendChild(checkBtn);
+    btnDiv.appendChild(delBtn);
+    parent.appendChild(div);
+
+});
