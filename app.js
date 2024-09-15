50752f050c73383dd82f569b4e806785387b3aab
Migeen Lamichhane Magar <migeen999@gmail.com>
2024-09-15 23:08:05 +0545

    bug corrected hurray

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const delSvg = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0
 const checkSvg = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#424242"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>'
 const checkButton = document.querySelectorAll('.check');
 const container = document.querySelector('.List');
+const inp = document.querySelector('input');
 // Implementing an Event Handler function for invoking a button click
 
 let Lists = [
@@ -23,12 +24,15 @@ function task(task, mark) {
 }
 
 const saveData = () => {
-    localStorage.setItem("data",container.innerHTML);
+    localStorage.setItem("data",JSON.stringify(Lists));
 }
 
 const loadData = () => {    
-    container.innerHTML = localStorage.getItem("data");
+    if(localStorage.data != null){
+        Lists = JSON.parse(localStorage.getItem("data"));
+    }
 }
+
 
 const toDoList = () => {
 
@@ -48,18 +52,16 @@ const toDoList = () => {
         btnDiv.classList.add('btns');
 
         let delBtn = document.createElement('Button');
-        delBtn.classList.add(`del-${index}`);
         delBtn.classList.add('del')
-        delBtn.id = index;
+        delBtn.setAttribute('get-index',index);
         delBtn.innerHTML = delSvg;
 
         let checkBtn = document.createElement('Button');
-        checkBtn.classList.add(`check-${index}`);
         checkBtn.classList.add('check');
-
+        checkBtn.setAttribute ('get-index',index);
         checkBtn.innerHTML = checkSvg;
 
-        if (x.mark == 'yes') {
+        if (x.mark === 'yes') {
             content.style.textDecoration = 'line-through';
         } else {
             content.style.textDecoration = '';
@@ -72,25 +74,23 @@ const toDoList = () => {
         btnDiv.appendChild(delBtn);
         container.appendChild(div);
 
-
+        
         checkBtn.addEventListener('click', (e) => {
-            console.log(e.target.classList);
-            if (x.mark === 'yes') {
-                x.mark = 'no';
+            let elementIndex = e.currentTarget.getAttribute('get-index');
+            if (Lists[elementIndex].mark === 'yes') {
+                Lists[elementIndex].mark = 'no';
                 content.style.textDecoration = '';
             } else {
-                x.mark = 'yes';
+                Lists[elementIndex].mark = 'yes';
                 content.style.textDecoration = 'line-through';
             }
             saveData();
         });
 
 
-        delBtn.addEventListener('click', () => {
-            let delclass = delBtn.className;
-            let index = delclass.match(/del-(\d+)/)[1];
-            saveData();
-            deleteTask(parseInt(index));
+        delBtn.addEventListener('click', (e) => {
+            let index = e.currentTarget.getAttribute('get-index');
+            deleteTask(parseInt(index));    
         })
         index++;
     }
@@ -98,7 +98,16 @@ const toDoList = () => {
 
 toDoList();
 
-addBtn.addEventListener('click', () => {
+inp.addEventListener('keypress',(event)=>{
+    if(event.key === 'Enter'){
+        event.preventDefault();
+        addBtn.click();
+    }
+});
+
+addBtn.addEventListener('click',addToList);
+
+function addToList(){
     const taskval = document.querySelector('input').value;
     const marker = 'no';
     const message = document.querySelector('.message');
@@ -112,14 +121,14 @@ addBtn.addEventListener('click', () => {
 
         const newTask = new task(taskval, marker);
         Lists.push(newTask);
-        toDoList();
         saveData();
+        toDoList();
     }
-})
+}
 
 const deleteTask = (index) => {
     Lists.splice(index, 1);
-    loadData();
+    saveData();
     toDoList();
 }
 
@@ -127,12 +136,3 @@ window.onload = ()=>{
     loadData();
     toDoList();
 }
-
-// const tempList = new Map();
-
-// tempList.set(1,'task1');
-// tempList.set(2,'task2');
-
-// console.log(tempList.get(2));
-
-// tempList.delete(2);
